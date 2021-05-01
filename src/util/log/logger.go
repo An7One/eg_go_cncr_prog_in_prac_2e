96e0e563f38bfff1b5e4bca3c0bd171dab3206d8
Leon <yanglyu.leon.7@gmail.com>
2021-04-30 23:58:08 -0700

    review

--- src/util/log/logger.go
+++ src/util/log/logger.go
@@ -1,7 +1,9 @@
 package log
 
 import (
+	"fmt"
 	"io"
+	"os"
 	"sync"
 
 	"an7one.com/eg_go_cncr_prog_in_prac_2e/src/util/log/base"
@@ -19,14 +21,50 @@ var loggerCreatorMap = map[base.LoggerType]LoggerCreator{}
 
 var rwm sync.RWMutex
 
-func RegisterLogger() error {
-
+// RegisterLogger register the logger
+func RegisterLogger(
+	loggerType base.LoggerType,
+	creator LoggerCreator,
+	cover bool) error {
+	if loggerType == "" {
+		return fmt.Errorf("logger reigster error: invalid logger type")
+	}
+	if creator == nil {
+		return fmt.Errorf("logger register error: invalid logger creator (logger type: %s)", loggerType)
+	}
+	rwm.Lock()
+	defer rwm.Unlock()
+	if _, ok := loggerCreatorMap[loggerType]; ok || !cover {
+		return fmt.Errorf("logger register error: already existing logger for type %q", loggerType)
+	}
+	loggerCreatorMap[loggerType] = creator
+	return nil
 }
 
+// DLogger returns a new default logger
 func DLogger() base.MyLogger {
-
+	return Logger(
+		base.TYPE_LOGRUS,
+		base.LEVEL_INFO,
+		base.FORMAT_TEXT,
+		os.Stdout,
+		nil)
 }
 
-func Logger() base.MyLogger {
-
+func Logger(
+	loggerType base.LoggerType,
+	level base.LogLevel,
+	format base.LogFormat,
+	writer io.Writer,
+	options []base.Option) base.MyLogger {
+	var logger base.MyLogger
+	rwm.RLock()
+	creator, ok := loggerCreatorMap[loggerType]
+	rwm.RUnlock()
+	if ok {
+		logger = creator(level, format, writer, options)
+	} else {
+		logger = logrus.NewLoggerBy(level, format, writer, options)
+	}
+	return logger
 }
--- src/util/log/logrus/logrus.go
+++ src/util/log/logrus/logrus.go
@@ -1,1 +1,26 @@
 package logrus
+
+import (
+	"an7one.com/eg_go_cncr_prog_in_prac_2e/src/util/log/base"
+
+	"github.com/Sirupsen/logrus"
+)
+
+type loggerLogrus struct {
+	level           base.LogLevel
+	format          base.LogFormat
+	optWithLocation base.OptWithLocation
+	inner           *logrus.Entry
+}
+
+func NewLogger() base.MyLogger {
+	return NewLoggerBy()
+}
+
+func NewLoggerBy() base.MyLogger {
+
+}
+
+func initInnerLogger() *logrus.Entry {
+
+}
